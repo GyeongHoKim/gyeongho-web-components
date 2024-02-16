de20432f5345ffd7d4cdc7649da492b956a82c7a
GyeongHoKim <rlarudgh2017@gmail.com>
2024-02-16 20:16:35 +0900

    [gh-divider](GWC-1) feat: divider 기능 추가

---
+++ src/base.style.js
@@ -0,0 +1,11 @@
+import {css} from 'lit';
+
+export const baseStyle = css`
+  :host {
+    --gh-border-color: black;
+    --gh-border-width: 1px;
+    --gh-spacing-small: 1rem;
+    --gh-spacing-medium: 2rem;
+    --gh-spacing-large: 4rem;
+  }
+`;
